524f61aabad251aece7ffad7120d8b88c876f2fd
AkshaySubbaram <subbaramakshay@gmail.com>
2025-08-26 15:33:16 +0530

    test application config

--- src/test/java/com/example/batch/load/LoadApplicationTests.java
+++ src/test/java/com/example/batch/load/LoadApplicationTests.java
@@ -1,9 +1,15 @@
 package com.example.batch.load;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@SpringBootTest
+@ActiveProfiles("test")
+@ImportAutoConfiguration(exclude = {
+		org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class
+})
 class LoadApplicationTests {
 
 	@Test
