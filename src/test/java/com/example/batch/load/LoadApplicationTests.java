d4451ac6b6445a0583850a92d7553eba8206de99
AkshaySubbaram <subbaramakshay@gmail.com>
2025-08-26 14:37:39 +0530

    testcase

--- src/test/java/com/example/batch/load/LoadApplicationTests.java
+++ src/test/java/com/example/batch/load/LoadApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.batch.load;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 class LoadApplicationTests {
 
 	@Test
